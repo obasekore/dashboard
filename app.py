1a57e7fb8e0350a9a5845ee4706dcd22979288d0
Czar-dev-2 <56700690+Czar-dev-2@users.noreply.github.com>
2025-05-01 17:10:14 +0900

    Initial Commits

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config("Dashboard")
+
+st.title("Welcome")
+
+
+# conn = st.connection("my_database")
+# df = conn.query("select * from plans")
+# st.dataframe(df)
